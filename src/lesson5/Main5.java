3d3e3ac2e3783ce1a4108ad5b7ffd16471011780
Maria <bmf_@mail.ru>
2019-09-11 10:22:28 +0300

    Recursion

--- src/lesson5/Main5.java
+++ src/lesson5/Main5.java
@@ -1,5 +1,9 @@
 package lesson5;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main5 {
 
     public static void main(String[] args) {
@@ -7,8 +11,15 @@ public class Main5 {
 //        System.out.println(sum(4356));
 //        System.out.println(sumRec(4356));
 //        print2(4);
-        print3(1, 5);
+//        print3(1, 5);
+//        oddNum();
+//        oddNum2();
+//        System.out.println(findMax());
+//        average2(0, 0);
+//        int[] maxCount = findMaxCount();
+//        System.out.println(maxCount[0] + " " + maxCount[1]);
     }
+
 
     public static String isExp(int n) {
         if (n == 2) {
@@ -67,6 +78,103 @@ public class Main5 {
         if (a > b) {
             System.out.println(a);
             print3(a - 1, b);
+        }
+    }
+
+    public static void oddNum() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        try {
+            n = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (n == 0) {
+            return;
+        }
+        if (n % 2 != 0) {
+            System.out.println(n);
+        }
+        oddNum();
+    }
+
+    public static void oddNum2() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        try {
+            n = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (n == 0) {
+            return;
+        }
+        if (n % 2 != 0) {
+            System.out.println(n);
+        }
+        oddNum2();
+    }
+
+    public static int findMax() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        try {
+            n = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (n == 0) {
+            return 0;
+        }
+        int maxForNextNumbers = findMax();
+        if (n > maxForNextNumbers) {
+            return n;
+        }
+        return maxForNextNumbers;
+    }
+
+    public static void average2(int sum, int count) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        try {
+            n = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (n > 0) {
+            average2(sum + n, ++count);
+        } else if (sum > 0 && count > 0) {
+            System.out.println(sum / count);;
+        }
+    }
+
+    public static int[] findMaxCount() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = 0;
+        try {
+            n = Integer.parseInt(reader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        int[] maxForNextElement;
+        if (n == 0) {
+            maxForNextElement = new int[2];
+            maxForNextElement[0] = 0;
+            maxForNextElement[1] = 1;
+            return maxForNextElement;
+        }
+
+        maxForNextElement = findMaxCount();
+        if (n > maxForNextElement[0]) {
+            maxForNextElement[0] = n;
+            maxForNextElement[1] = 1;
+            return maxForNextElement;
+        } else if (n == maxForNextElement[0]) {
+            maxForNextElement[1] = maxForNextElement[1] + 1;
+            return maxForNextElement;
+        } else {
+            return maxForNextElement;
         }
     }
 }
