bccb4703adf4992a54a63e9168f5c064c9b5c953
Maria <bmf_@mail.ru>
2019-09-18 12:54:34 +0300

    Lesson 7

---
+++ src/lesson7/Vertex.java
@@ -0,0 +1,28 @@
+package lesson7;
+
+public class Vertex {
+
+    private final String label;
+    private boolean visited;
+
+    public Vertex(String label) {
+        this.label = label;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" + "label='" + label + '\'' + "}";
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+}
